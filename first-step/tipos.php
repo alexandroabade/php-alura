f05d58b834232134b98de13acddd81bc595e1f56
alexandroabade <alexandroabade@gmail.com>
2022-05-26 18:37:09 -0300

    working data types

---
+++ first-step/tipos.php
@@ -0,0 +1,16 @@
+<?php
+
+$idade = 35;
+$salario = 1000.30;
+$divisao = 10 / 3;
+
+// echo gettype($divisao);
+// echo $divisao;
+
+$texto = "Olá mundo!";
+// echo gettype($texto);
+
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($verdadeiro);
