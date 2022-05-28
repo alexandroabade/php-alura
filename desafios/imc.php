6bf9b843c4ba0014bdcc052d0fdd4a2e1fdc1910
alexandroabade <alexandroabade@gmail.com>
2022-05-28 11:48:41 -0300

    add calculate imc done

--- desafios/imc.php
+++ desafios/imc.php
@@ -1,12 +1,12 @@
 <?php
 
-$peso = 78;
+$peso = 60;
 $altura = 1.94;
 $imc = $peso / $altura ** 2;
 
 echo "Seu IMC é de $imc. Você está com o IMC ";
 
-if ($imc < 18.5) {
+if ($imc < 18) {
     echo "abaixo";
 } elseif ($imc < 25) {
     echo "dentro";
