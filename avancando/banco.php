385597f74f115a39759da8dd28eb66efa31a17b3
alexandroabade <alexandroabade@gmail.com>
2022-06-10 11:19:36 -0300

    implemented the deposit and withdraw function

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,18 +1,5 @@
 <?php
 
-function sacar($conta, $valorASacar) {
-  if ($valorASacar > $conta ['saldo']) {
-    exibeMensagem("Você não pode sacar");
-  } else {
-    $conta['saldo'] -= $valorASacar;
-  }
-  return $conta;
-}
-
-function exibeMensagem($mensagem) {
-  echo $mensagem . PHP_EOL;
-}
-
 $contasCorrentes = [
   '123.456.789-10' => [
     'titular' => 'Maria',
@@ -28,8 +15,45 @@ $contasCorrentes = [
   ]
 ];
 
-$contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 500);
-$contasCorrentes['123.456.689-11'] = sacar($contasCorrentes['123.456.689-11'], 500);
+function exibeMensagem(string $mensagem) 
+{
+  echo $mensagem . PHP_EOL;
+}
+
+function depositar(array $conta, float $valorADepositar): array 
+{
+  if ($valorADepositar > 0) {
+      $conta['saldo'] += $valorADepositar;
+  } else {
+    exibeMensagem("Depositos precisam ser positivos");
+  }
+  return $conta;
+}
+
+function sacar( array $conta, float $valorASacar): array 
+{
+  if ($valorASacar > $conta['saldo']) 
+  {
+    exibeMensagem("Você não pode sacar");
+  } else {
+    $conta['saldo'] -= $valorASacar;
+  }
+  return $conta;
+}
+
+$contasCorrentes['123.456.789-10'] = sacar(
+  $contasCorrentes['123.456.789-10'], 
+  500
+);
+$contasCorrentes['123.456.689-11'] = sacar(
+  $contasCorrentes['123.456.689-11'], 
+  200
+);
+
+$contasCorrentes['123.256.789-12'] = depositar(
+  $contasCorrentes['123.256.789-12'], 
+  -300
+);
 
 foreach ($contasCorrentes as $cpf => $conta) {
   exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
